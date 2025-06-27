527f39a600211130e1af6bc46f46ae9e7ad1209a
fenrisu1fr <zitong.tian@outlook.com>
2025-06-27 09:35:14 +0800

    fix

--- src/client/tls_stream.rs
+++ src/client/tls_stream.rs
@@ -1,22 +1,22 @@
-use crate::Config;
 use futures_util::io::{AsyncRead, AsyncWrite};
-
-#[cfg(feature = "native-tls")]
-mod native_tls_stream;
+use crate::client::Config;
 
 #[cfg(feature = "rustls")]
 mod rustls_tls_stream;
 
-#[cfg(feature = "vendored-openssl")]
-mod opentls_tls_stream;
+#[cfg(all(feature = "native-tls", not(feature = "rustls")))]
+mod native_tls_stream;
 
-#[cfg(feature = "native-tls")]
-pub(crate) use native_tls_stream::TlsStream;
+#[cfg(all(feature = "vendored-openssl", not(feature = "rustls"), not(feature = "native-tls")))]
+mod opentls_tls_stream;
 
 #[cfg(feature = "rustls")]
 pub(crate) use rustls_tls_stream::TlsStream;
 
-#[cfg(feature = "vendored-openssl")]
+#[cfg(all(feature = "native-tls", not(feature = "rustls")))]
+pub(crate) use native_tls_stream::TlsStream;
+
+#[cfg(all(feature = "vendored-openssl", not(feature = "rustls"), not(feature = "native-tls")))]
 pub(crate) use opentls_tls_stream::TlsStream;
 
 #[cfg(feature = "rustls")]
@@ -27,7 +27,7 @@ pub(crate) async fn create_tls_stream<S: AsyncRead + AsyncWrite + Unpin + Send>(
     TlsStream::new(config, stream).await
 }
 
-#[cfg(feature = "native-tls")]
+#[cfg(all(feature = "native-tls", not(feature = "rustls")))]
 pub(crate) async fn create_tls_stream<S: AsyncRead + AsyncWrite + Unpin + Send>(
     config: &Config,
     stream: S,
@@ -35,7 +35,7 @@ pub(crate) async fn create_tls_stream<S: AsyncRead + AsyncWrite + Unpin + Send>(
     native_tls_stream::create_tls_stream(config, stream).await
 }
 
-#[cfg(feature = "vendored-openssl")]
+#[cfg(all(feature = "vendored-openssl", not(feature = "rustls"), not(feature = "native-tls")))]
 pub(crate) async fn create_tls_stream<S: AsyncRead + AsyncWrite + Unpin + Send>(
     config: &Config,
     stream: S,
